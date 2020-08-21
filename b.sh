54d122214dbfb795a2b27448abd8dfd3874fc590
lesacar <69954403+lesacar@users.noreply.github.com>
2020-08-21 21:38:06 +0200

    Add files via upload

---
+++ b.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "-- Choose Desktop
+read -p "Type 'plasma kde-applications' for KDE --< KDE looks similar to windows, users coming from windows might find it easier to use >-- type 'gnome gnome-extra' for GNOME --< looks similar to macOS, comes by defalt on Ubuntu >--" de
