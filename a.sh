a6211b59b4d14280cee9ef06c58d502c246d3b88
lesacar <69954403+lesacar@users.noreply.github.com>
2020-12-13 23:10:35 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+bootype=bios
+ls /sys/irmware/efi/efivars >/dev/null 2>&1 && bootype=efi
 echo "This is my Arch Linux install script"$'\n'"it works only on UEFI systems"$'\n'"If you want the legacy BIOS version check my repoitory"
 echo "Set keyboard layout (only croatian and german)"
 read -p "Type 'de' for german or 'croat' for croatian: >> " lang1
 loadkeys $lang1
+clear
 echo "Partition disk tool"
 echo "You must choose GPT when a prompt shows up"
 sleep 1
