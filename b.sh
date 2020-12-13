8e579c0ae468b9c129c7ed5dc87abf4ea7f6a86e
lesacar <69954403+lesacar@users.noreply.github.com>
2020-12-13 23:43:40 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "$bootype is the boot type..."
+sleep 5
+clear
 echo "-- Choose Desktop --"
 echo "The Desktop enviroment is the graphical interface you will be interacting with (all the icons, start menu, task bar, etc...)"
 echo "You can leave the below blank if you have your own build of a graphical interface or you can typeyour choice if it isn't listed"
@@ -7,14 +10,14 @@ pacman -Syyu --noconfirm
 pacman -S networkmanager xorg xorg-xinit man-db man-pages linux-headers $de --noconfirm
 systemctl enable NetworkManager
 systemctl start NetworkManager
-echo "--------"
+clear
 echo "Installing the sddm display manager"
 pacman -S sddm --noconfirm
 systemctl enable sddm
-echo "--------"
+clear
 echo "Select timezone"
-read -p "Timezone - Region >> " reg
-read -p "Timezone - City >> " cit
+read -p "What Region are you in? ( Europe, America, etc ) >> " reg
+read -p "What City are you in? ( London, NewYork, Toyko, etc) >> " cit
 ln -sf /usr/share/zoneinfo/$reg/$cit /etc/localtime
 hwclock --systohc
 echo "Set hardware clock to UTC"
@@ -23,29 +26,37 @@ cp /etc/locale.gen /etc/locale.gen.backup
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
+clear
 echo "Setting network settings"
 read -p "Enter hostname for computer (computer name): >> " hostnm
 sleep 1
 echo "$hostnm" >> /etc/hostname
 echo "127.0.0.1		localhost"$'\n'"::1		localhost"$'\n'"127.01.1	$hostnm.localdomain $hostnm" >> /etc/hosts
-echo "-------"
+clear
 echo "Change root password"
 read -s -p "New root password: >> " roopas
 echo "root:$roopas" | chpasswd
-echo "-------"
+echo "-----------------"
 echo "Create user"
 read -p "New user name: >> " nusrnm
 useradd -m -G wheel $nusrnm
 read -s -p "Create password for new user: >> " pswfnu
 echo "$nusrnm:$pswfnu" | chpasswd
-pacman -S grub efibootmgr --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+clear
+if [ "bootype" = "bios" ]; then
+  pacman -S grub efibootmgr --noconfirm && grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg;
+else
+  pacman -S grub --noconfirm && grub-install /dev/$rootp && grub-mkconfig -o /boot/grub/grub.cfg
+fi
 echo "You will need to specify what CPU brand you're using"
 read -p "Type 'amd' for amd or 'intel' for intel >> " cpu
 pacman -S $cpu-ucode --noconfirm
-echo "-------"
+clear
 echo "Install graphics driver"
-read -p "IF YOU ARE USING A NVIDIA GRAPHICS CARD YOU WILL NEED TO MANUALLY INSTALL THE DRIVER, READ THE GUIDE AFTER THE INSTALLATION HAS COMPLETED. Type amdgpu for amd graphics, intel for intel graphics, or vmware for vmware graphics" gpudrvrs
-pacman -S xf86-video-$gpudrvrs --noconfirm
-echo "---------------------------------"
+read -p "Type amdgpu for amd graphics, intel for intel graphics, nvidia for nvidia graphics, or vmware for vmware graphics > " gpudrvrs
+if [ "$gpudrvrs" = "nvidia" ]; then
+  pacman -S nvidia nvidia-utils --noconfirm;
+else
+  pacman -S xf86-video-$gpudrvrs --noconfirm
+fi
+clear
